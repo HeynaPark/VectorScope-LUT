ed3e3d23dd354cd0d0ca559bdf076182c85798c1
hnpark <hnpark@4dreplay.com>
2022-05-16 14:56:49 +0900

    VectorScope rendering fix end.

--- VectorScope/VectorScope.cpp
+++ VectorScope/VectorScope.cpp
@@ -104,36 +104,24 @@ void Vectorscope::DrawScope(Mat src, Mat scope)
 	Mat bgr[3];
 	Mat b, g, r;
 
-	Mat temp,img;
-	//resize(src, temp, Size(480, 270));
-
-
-	//temp.convertTo(img, CV_64F);
-	//split(img, bgr);
-	//b = bgr[0];
-	//g = bgr[1];
-	//r = bgr[2];
-
+	Mat temp, img;
 	resize(src, img, Size(480, 270));
 
 
-	//img.convertTo(img, CV_64F);
 	split(img, bgr);
-	bgr[0].convertTo(b, CV_64FC1);
-	bgr[1].convertTo(g, CV_64FC1);
-	bgr[2].convertTo(r, CV_64FC1);
+	bgr[0].convertTo(b, CV_32FC1);
+	bgr[1].convertTo(g, CV_32FC1);
+	bgr[2].convertTo(r, CV_32FC1);
 
 
 
 	int scale = 2;
-	_Cb = Mat(img.size(), CV_64F);
-	_Cr = Mat(img.size(), CV_64F);
+	_Cb = Mat(img.size(), CV_32F);
+	_Cr = Mat(img.size(), CV_32F);
 	_Y = (0.299 * r) + (0.587 * g) + (0.114 * b);
 	_Cb = (-0.169 * r) - (0.331 * g) + (0.499 * b) + 128.0 * scale;
 	_Cr = (0.499 * r) - (0.418 * g) - (0.0813 * b) + 128.0 * scale;
-	/*_Y = 16.0 + (65.481 * r + 128.553 * g + 24.966 * b);
-	_Cb = 128.0  +(- 37.797 * r - 74.203 * g + 112.0 * b);
-	_Cr = 128.0  +( + 112.0 * r - 93.786 * g - 18.214 * b);*/
+
 
 	_Cr = 256.0 * scale - _Cr;
 	//_Cb = 256*scale - _Cb;
@@ -142,36 +130,21 @@ void Vectorscope::DrawScope(Mat src, Mat scope)
 	Mat Cb, Cr;
 	_Cb.convertTo(Cb, CV_8UC1);
 	_Cr.convertTo(Cr, CV_8UC1);
-	
-	
-	//imshow("Cb", _Cb);
-	//imshow("Cr", _Cr);
 
 
-	                       
+
 	Point pos;
 	Mat scopebgr[3];
 	split(scope, scopebgr);
-	for (int row = 0; row < img.rows; row++) 
+	for (int row = 0; row < img.rows; row++)
 	{
 		for (int col = 0; col < img.cols; col++)
 		{
-			pos.x = int(_Cr.at<float>(row, col));
-			pos.y = int(_Cb.at<float>(row, col));
+			pos.y = int(_Cr.at<float>(row, col));
+			pos.x = int(_Cb.at<float>(row, col));
 
-			cout << pos << endl;
-		//	scope.push_back(pos)
-			//scope.at<Vec3b>(pos) = ((bgr[0].at<uchar>(row, col)) << 3) | ((bgr[1].at<uchar>(row, col)) << 2) | (bgr[2].at<uchar>(row, col)) << 1;
-		//	scope.at<Vec3b>(pos) = ((temp.at<Vec3b>(row, col)[0]) << 2) | ((temp.at<Vec3b>(row, col)[1]) << 2) | (temp.at<Vec3b>(row, col)[2]) << 0;
 			scope.at<Vec3b>(pos) = img.at<Vec3b>(row, col);
-			/*scope.at<Vec3b>(pos.x, pos.y)[0]  = src.at<Vec3b>(row, col)[0];
-			scope.at<Vec3b>(pos.x, pos.y)[1]  = src.at<Vec3b>(row, col)[1];
-			scope.at<Vec3b>(pos.x, pos.y)[2]  = src.at<Vec3b>(row, col)[2];*/
-			//scope.at<uchar>(pos.x, pos.y)[0] = temp.at<uchar>(row, col)[0];
 
-			//cout << to_string(src.at<Vec3b>(row, col)[0]) << endl;
-			//cout << to_string(src.at<Vec3b>(row, col)[1]) << endl;
-			//scope.at<Vec3b>(pos.x, pos.y) = src.at<Vec3b>(row, col)[0], src.at<Vec3b>(row, col)[1], src.at<Vec3b>(row, col)[2];
 		}
 	}
 
